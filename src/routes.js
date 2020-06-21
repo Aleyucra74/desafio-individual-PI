ed4a6a30b73ed8a84d7687d660a5b14804158f96
Alexandrey <alexandre.yucra@outlook.com>
2020-06-21 02:09:47 -0300

    validacao e estetica

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,22 @@ const { Router } = require('express');
 const MessageController = require('./app/controllers/MessageController');
 const UserController = require('./app/controllers/UserController');
 const { Message } = require('../src/app/models');
-
+const { celebrate, Joi } = require('celebrate');
 
 const routes = Router();
 
 routes.get('/show', MessageController.index);
 
-routes.post('/new', MessageController.store);
+routes.post('/new', celebrate({
+    body: Joi.object().keys({
+        name: Joi.string().required().error( new Error('Nome é necessario')),
+        email: Joi.string().required().email().error( new Error('Email é necessario')),
+        onde: Joi.string().required().error( new Error('Local é necessario')),
+        phone: Joi.number().required().error( new Error('Numero é necessario')),
+        message: Joi.string().required().error( new Error('Mensagem é necessario')),
+    })
+}, {
+    abortEarly:false
+}), MessageController.store);
 routes.post('/logar', UserController.auth);
 module.exports = routes;
